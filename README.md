19195d9efb81392ae6f4b9d8822ad770d18f390f
jefersonecruz-commits <jeferson.e.cruz@aluno.senai.br>
2025-10-01 16:58:38 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # forja-tarde-logica-01
-primeiro repositorio da turma
+primeiro repositorio da turma 
+
+label = escreva
+h1 = titulo 
+input = leia 
+git clone = "nome do repositorio" 
---
+++ logica.js
@@ -0,0 +1,8 @@
+function soma(){
+    var numero1 = document.getElementById("n3").valueAsNumber
+    var numero2 = document.getElementById("n4").valueAsNumber
+    var resposta = document.getElementById("respsub")
+
+    resposta.textContent = numero1 - numero2
+    // console.log(numero1, numero2)
+}
