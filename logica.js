5303006887bff51aaa0d1092ba90b137bceeb9f3
jefersonecruz-commits <jeferson.e.cruz@aluno.senai.br>
2025-10-02 16:51:59 -0300

    Add files via upload

--- logica.js
+++ logica.js
@@ -1,8 +1,23 @@
 function soma(){
+    var numero1 = document.getElementById("n1").valueAsNumber
+    var numero2 = document.getElementById("n2").valueAsNumber
+    var resposta = document.getElementById("respsoma")
+
+    resposta.textContent = numero1 + numero2
+}function soma(){
     var numero1 = document.getElementById("n3").valueAsNumber
     var numero2 = document.getElementById("n4").valueAsNumber
     var resposta = document.getElementById("respsub")
 
     resposta.textContent = numero1 - numero2
-    // console.log(numero1, numero2)
+}function div(){
+    var a = parseFloat(document.getElementById("n7").value)
+    var b = parseFloat(document.getElementById("n8").value)
+    var resp = document.getElementById("respDiv")
+
+    if(b !== 0){
+        resp.innerHTML = (a / b).toFixed(2)
+    }else(
+        resp.innerHTML = "não se divide por 0"
+    )
 }
